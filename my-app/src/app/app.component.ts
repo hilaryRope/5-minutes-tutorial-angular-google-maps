5686d11a91910ffe01de7a8e9bfec760446b9dbe
innuendo <corda.ilaria@gmail.com>
2017-10-06 10:36:15 +0100

    ADD all the project's files

--- my-app/src/app/app.component.ts
+++ my-app/src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+ selector: 'app-root',
+ templateUrl: 'app.component.html',
+ styleUrls: ['app.component.css'],
 })
 export class AppComponent {
-  title = 'app';
+ title: string = 'My first angular2-google-maps project';
+ lat1: number = 51.531826;
+ lng1: number = -0.124391;
+ lat2: number = 51.511899;
+ lng2: number = -0.123270;
 }
--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -1,16 +1,23 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
+import { NgModule, ApplicationRef } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 
+import { AgmCoreModule } from 'angular2-google-maps/core';
+
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+ imports: [
+ BrowserModule,
+ CommonModule,
+ FormsModule,
+ AgmCoreModule.forRoot({
+ apiKey: 'MY_API_KEY'
+ })
+ ],
+ providers: [],
+ declarations: [
+ AppComponent
+ ],
+ bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
